239b6cf5435bb3c9a8902437b1f08b41776e5b30
Thyrst <thyrst@seznam.cz>
2020-05-08 22:49:11 +0200

    firebase

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+import UserApi from '../api/UserApi';
+
+
+function log_doc(querySnapshot) {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        console.log(doc.data());
+    });
+}
+
 
 export default function Table() {
+    UserApi.get_users().then(log_doc);
     return (
         <div>
             test
